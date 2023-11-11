b586a76f025b77f118665e78b5c9f0a1d826c24f
anghelmihai2002 <Anghel.Ma.Mihai@student.utcluj.ro>
2023-11-11 21:17:48 +0200

    resolved #28

---
+++ kickoffpicks/frontend/src/Home.js
@@ -0,0 +1,18 @@
+import Navbar from "./components/navbar";
+import "./Home.css";
+
+const GameModes = () => {
+  return (
+    <div class="GameModes">
+      <p>asdfasfdasdf</p>
+    </div>
+  );
+};
+
+export default function HomeExport() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
