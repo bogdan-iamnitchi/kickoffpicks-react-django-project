1790c9dd9955948787a8ba1c6546cd02b418e416
anghelmihai2002 <Anghel.Ma.Mihai@student.utcluj.ro>
2023-11-11 16:42:11 +0200

    resolve #13

--- kickoffpicks/frontend/src/App.js
+++ kickoffpicks/frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import React, {Component} from 'react'
+import Navbar from "./components/navbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
